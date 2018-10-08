18a78aed3f61507f5003ca2666511082dec22d09
Sergio Vacas <vaks@hotmail.es>
2018-10-08 14:26:55 +0200

    añadida barra de navegación

--- javaScript/Sesion.js
+++ javaScript/Sesion.js
@@ -3,16 +3,16 @@ var usuarioActual;
 function SignIn(){
 	localStorage['dni']="12345678A";
 	window.location.href = "./web/perfilUsuario.html";
+	//document.getElementById('container').load("../web/perfilUsuario.txt"); 
 	
 }
 
 function mostrarDatosUsuario(){
 	firebase.database().ref('Usuarios').child(localStorage['dni']).on('value',function(snapshot) {
 		usuarioActual=snapshot.val();
-
 		var output="<p> Nombre: "+ usuarioActual.nombre+" </p>";
-		output=output+"<p> Apellido: "+ usuarioActual.apellido+" </p>";	
-		document.getElementById('content').innerHTML=output;
+		output=output+"<p> Apellido: "+ usuarioActual.apellido+" </p>";
+		document.getElementById('container').innerHTML=output;
 	});
 	
 }
