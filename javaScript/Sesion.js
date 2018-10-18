4d776ffe233f8228fb6402970547b6cf28ddff9a
pable3 <pgallardo9@gmail.com>
2018-10-18 20:15:15 +0200

    Promocion
    Añadido botón para peomocionar anuncios

--- javaScript/Sesion.js
+++ javaScript/Sesion.js
@@ -17,3 +17,25 @@ function mostrarDatosUsuario(){
 	
 }
 
+function promocionarUsuario(){
+	//Consulta los tokens del usuario
+	let tokens = new Promise((resolve, reject) => {
+		firebase.database().ref('Usuarios').child(localStorage['dni']).on('value',function(snapshot) {
+			usuarioActual=snapshot.val();
+			resolve(parseInt(usuarioActual.tokens));
+		});
+	});
+
+	//Una vez se han consultado, se restan 5 tokens y se convierte el anuncio en premium
+	tokens.then((res) => {
+		if(res >= 5){
+				firebase.database().ref('Usuarios').child(localStorage['dni']).child('tokens').set(res - 5);
+				firebase.database().ref('Anuncios').child(localStorage['dni']).child('premium').set(true);
+				alert("Tu anuncio ha sido promocionado.");
+
+		} else {
+				alert("No tienes suficientes tokens.");
+		}
+	});
+}
+
