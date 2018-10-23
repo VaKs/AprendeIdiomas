7b798f83dcf0b0f60893fa76cd03cbe4234fb8dd
Sergio Vacas <vaks@hotmail.es>
2018-10-23 11:59:09 +0200

    añadido notificar al alumno cuando se cancela la clase

--- javaScript/Manejoclases.js
+++ javaScript/Manejoclases.js
@@ -63,26 +63,22 @@ function notificar(decision){
 */
 function listar_notificaciones(){
 	
-	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').once('value').then(function(snapshot) {
-	var n = document.getElementById('container1').innerHTML;
-	 
-	document.getElementById('container1').innerHTML= n + "<ul>";
+	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').on('value',function(snapshot) {
+	var output = "<p> Notificaciones: </p><br><ul>";
 	snapshot.forEach(notificacionSnapshot => {
 		var key = notificacionSnapshot.key;
 		var notificacion = notificacionSnapshot.val();
-		var claseout = "<li>"+ notificacion.descripcion;
+		output =output+ "<li>"+ notificacion.descripcion;
 		if(notificacion.tipo == "clase"){
 			
-			claseout = claseout + ": " + notificacion.fecha +" a las  "+ notificacion.hora +" de " + notificacion.idioma + " por " + notificacion.solicitante +
+			output = output + ": " + notificacion.fecha +" a las  "+ notificacion.hora +" de " + notificacion.idioma + " por " + notificacion.solicitante +
 			"<br><button id='botonaceptar' class='btn btn-success' onclick='aceptar_clase(\""+key+"\")'>Aceptar</button> "+
 			"<button id='botonrechazar' class='btn btn-danger' onclick='rechazar_clase(\""+key+"\")'>Rechazar</button>";
 		}
-	    claseout = claseout + "</li>";
-		n = document.getElementById('container1').innerHTML;
-		document.getElementById('container1').innerHTML = n + claseout;
+	    output = output + "</li>";
 	});
-	n = document.getElementById('container1').innerHTML;
-		document.getElementById('container1').innerHTML = n + "</ul>";
+		output = output + "</ul>";
+		document.getElementById('container1').innerHTML = output;
 	});
 	
 }
@@ -91,15 +87,21 @@ function aceptar_clase(key){
 	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).child('estado').set('aceptada');
 	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).once('value').then(function(snapshot) {
 		var notificacion = snapshot.val();
-	var clase = new Object();
-	
-	clase.dnialumno = notificacion.solicitante;
-	clase.fecha = notificacion.fecha;
-	clase.hora = notificacion.hora;
-	clase.idioma = notificacion.idioma;
-	clase.precio = notificacion.precio;
-	firebase.database().ref('Usuarios').child(localStorage['dni']).child('clases').push(clase);
-	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).remove();
+		
+		var clase = new Object();
+		clase.dnialumno = notificacion.solicitante;
+		clase.fecha = notificacion.fecha;
+		clase.hora = notificacion.hora;
+		clase.idioma = notificacion.idioma;
+		clase.precio = notificacion.precio;
+		firebase.database().ref('Usuarios').child(localStorage['dni']).child('clases').push(clase);
+		firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).remove();
+		
+		var notificacionAceptacion = new Object();
+		notificacionAceptacion.descripcion = "Se ha aceptado la solicitud para la clase de "+notificacion.idioma+" en fecha: "+notificacion.fecha+"-"+notificacion.hora;
+		
+		notificarUsuario(notificacion.solicitante,notificacionAceptacion);
+		
 	});
 	
 	
@@ -107,9 +109,21 @@ function aceptar_clase(key){
 }
 
 function rechazar_clase(key){
+	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).once('value').then(function(snapshot) {
+		var notificacion = snapshot.val();
+		var notificacionRechazada = new Object();		
+		notificacionRechazada.descripcion = "Se ha rechazado la solicitud para la clase de "+notificacion.idioma+" en fecha: "+notificacion.fecha+"-"+notificacion.hora;
+		
+		notificarUsuario(notificacion.solicitante,notificacionRechazada);
+
+		firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).child('estado').set('rechazada');
+		firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).remove();
+	});	
+}
+
+function notificarUsuario(dni,notificacion){
+	firebase.database().ref('Usuarios').child(dni).child('notificaciones').push(notificacion);
 	
-	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).child('estado').set('rechazada');
-	firebase.database().ref('Usuarios').child(localStorage['dni']).child('notificaciones').child(key).remove();
 }
 
 
